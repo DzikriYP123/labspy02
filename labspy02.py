7f1e57c823a4496b9f4cf934a03ab1ee9184b8a4
DzikriYP123 <dzikri.iluam@gmail.com>
2024-10-24 21:48:00 +0700

    Add files via upload
    program python nya

---
+++ labspy02.py
@@ -0,0 +1,15 @@
+a = input("Masukkan bilangan A: ")
+b = input("Masukkan bilangan B: ")
+c = input("Masukkan bilangan C: ")
+print ("nilai bilangan a=",a)
+print ("nilai bilangan b=",b)
+print ("nilai bilangan c=",c)
+
+if a>b and a>c :
+    print("bilangan terbesar=",a)
+elif b>a and b>c :
+    print("bilangan terbesar=",b)
+elif c>a and c>b:
+    print("bilangan terbesar=",c)
+
+
